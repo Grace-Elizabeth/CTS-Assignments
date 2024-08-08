ac8d19c112915380c3afee334f285e22fce6500a
Grace-Elizabeth <graceelizabethds15@gmail.com>
2024-08-08 19:44:05 +0530

    Add files via upload

---
+++ week 2/week 2 - Spring Core Maven/LibraryManagement/src/main/java/com/library/LibraryManagement/BookRepository.java
@@ -0,0 +1,7 @@
+package com.library.LibraryManagement;
+
+public class BookRepository {
+    public void getAllBooks() {
+        System.out.println("Fetching all books from the library...");
+    }
+}
---
+++ week 2/week 2 - Spring Core Maven/LibraryManagement/src/main/java/com/library/LibraryManagement/BookService.java
@@ -0,0 +1,7 @@
+package com.library.LibraryManagement;
+
+public class BookService {
+    public void displayWelcomeMessage() {
+        System.out.println("Welcome to the Library Management System!");
+    }
+}
